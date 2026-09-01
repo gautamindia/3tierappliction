bce75696d66a794a75f0d94ca2cf3a2c1f4fa439
gautam saini <you@example.com>
2026-09-01 05:21:52 +0000

    v37 ami id change

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_instance" "this" {
-  ami                         = var.ami_id != null ? var.ami_id : data.aws_ami.amazon_linux[0].id
+  ami                         = var.ami_id 
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.vpc_security_group_ids
